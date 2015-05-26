ac168e2e184cd72b3206d5e8dae0f30c4c5e943d
luki1413 <luki13@gmail.com>
2015-05-26 22:01:49 +0200

    20150526_bootstrap.css-poprawione

--- MvcGalleryBootstrap3/App_Start/BundleConfig.cs
+++ MvcGalleryBootstrap3/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace MvcGalleryBootstrap3
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery*"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
